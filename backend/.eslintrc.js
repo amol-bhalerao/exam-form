59c25997ddd722f3aa0c6e654524594d64fc0c88
codelangs <codelangsaurangabad@gmail.com>
2026-03-28 01:01:51 +0530

    Optimize backend: move @types to devDependencies, add Prettier and ESLint config

---
+++ backend/.eslintrc.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-arrow-callback': 'warn',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+    },
+  },
+];
